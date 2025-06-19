b79718ba8570c0b4ffe8bcc5839108a95cfe4337
kitsadakorn ketkaeo <el2212@signalschool.ac.th>
2025-06-19 15:37:38 +0700

    first commit

---
+++ w_06.py
@@ -0,0 +1,8 @@
+# List
+nambers = []
+colors = ["red","Green","Blue","Yellow"]
+nambers = [11,10,99,45,33,11]
+nambers_1_20 = list(range(1,20))
+# nambers_1_100 = list(range(1,101,1))
+nambers_1_100 = list(range(1,101,2))
+print(nambers_1_100)
